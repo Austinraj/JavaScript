8657e92da813703eeb1b4575033692db2b68b9e8
Ausatinraj <malaraustinraja25@gmail.com>
2023-10-14 16:35:17 +0530

    commit

--- day 4/day 4 task/1_task.js
+++ day 4/day 4 task/1_task.js
@@ -1,12 +1,12 @@
-/*1.Do the below programs in anonymous function & IIFE
-a.Print odd numbers in an array
-b.Convert all the strings to title caps in a string array
-c.Sum of all numbers in an array
-d.Return all the prime numbers in an array
-e.Return all the palindromes in an array
-f.Return median of two sorted arrays of the same size.
-g.Remove duplicates from an array
-h.Rotate an array by k times */
+// 1.Do the below programs in anonymous function & IIFE
+// a.Print odd numbers in an array
+// b.Convert all the strings to title caps in a string array
+// c.Sum of all numbers in an array
+// d.Return all the prime numbers in an array
+// e.Return all the palindromes in an array
+// f.Return median of two sorted arrays of the same size.
+// g.Remove duplicates from an array
+// h.Rotate an array by k times 
 
 //a.
 const array = [1,2,3,4,5,6,7,8,9,10];
@@ -71,43 +71,68 @@ const number4 = function(){//anonymous function
 
 
 
+//e.
+
+let str1 =  "madam";
+
+var string3 = function (){
+    let s = str1.split('').reverse().join('');
+   // console.log(s);
+    let a = s;
+    if (str1 == a){
+        console.log(a);
+    }else{
+        console.log("not palindrome string")
+    }
+
+}
+string3();
+
+//f.
 
 
+//g.
+var arr = ["car", "bus","bike", "car","bicycle", "bike", ]
 
-/* 2.Do the below programs in arrow functions.
-a.Print odd numbers in an array
-b.Convert all the strings to title caps in a string array
-c.Sum of all numbers in an array
-d.Return all the prime numbers in an array
-e.Return all the palindromes in an array */
+var duplicates =  arr.filter((element, index) =>{
+        return arr.indexOf(element) == index
+    });
+duplicates.forEach(console.log);
+
+//  2.Do the below programs in arrow functions.
+// a.Print odd numbers in an array
+// b.Convert all the strings to title caps in a string array
+// c.Sum of all numbers in an array
+// d.Return all the prime numbers in an array
+// e.Return all the palindromes in an array 
 
 
 //a.
-const array5 = [1,2,3,4,5,6,7,8,9,10];
-var number = ()=>{//arrow function
-    for (i of array){
-        if(i %2 ==0){
-            console.log(i);
-        }
+// const array5 = [1,2,3,4,5,6,7,8,9,10];
+// var number = ()=>{//arrow function
+//     for (i of array){
+//         if(i %2 ==0){
+//             console.log(i);
+//         }
         
-    }
-}
-number();
+//     }
+// }
+// number();
 
-//b.
-var str = ()=>{
-    var name = "raja";
-    console.log(name.toUpperCase());
-}
-str();
+// //b.
+// var str = ()=>{
+//     var name = "raja";
+//     console.log(name.toUpperCase());
+// }
+// str();
 
-//c.
-let adding =0;
-var sum1 =()=>{
-    for(i=0;i<array5.length;i++){
+// //c.
+// let adding =0;
+// var sum1 =()=>{
+//     for(i=0;i<array5.length;i++){
        
-        adding +=array5[i];
-    }
-    console.log(adding);
-}
-sum1();
+//         adding +=array5[i];
+//     }
+//     console.log(adding);
+// }
+// sum1(); 
---
+++ day 6/script.js
@@ -0,0 +1,53 @@
+// Recap session
+
+var i = 1;
+i = i + 1;
+i++; // i + 1 only used panna mudiyum
+i += 1;//i++
+i *= 5;
+
+for( var i =1 ; i< 10 && 1 % 2 ; i+= 2){
+ console.log(i);
+}
+
+
+// + => addition
+// - => Subtraction
+// * => Mutiplication
+// / => Division
+// % => Reminder
+
+var i = 1;
+if (i>1){
+    console.log(i);
+
+}else{
+    console.log()
+}
+
+var t = 10;
+if (t >= 5 && t <=10){
+    console.log(t);
+}
+var t1 = 25;
+if (t1 >= 5 || t1 <=10){
+    console.log(t1);
+}
+
+// type Conversion
+
+var test = "1234";
+
+//number()
+//string()
+//Boolean()
+//parseInt()
+//parseFloat()
+
+console.log(typeof(test));
+console.log(typeof(+test));
+
+var str = "1243";
+number = number(str);
+
+console.log(number);

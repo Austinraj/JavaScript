786f3d77771c27c7944a0efefb216786ac4324fd
Austinraj <malaraustinraja25@gmail.com>
2023-10-24 20:12:04 +0530

    commit

--- day 17/index.js
+++ day 17/index.js
@@ -41,7 +41,34 @@ http.open("GET", "https://restcountries.com/v3.1/all");
 http.send();
 
 
+// . -> class #-> id 
+// getElementById - Object
+// getElementsByClassName - array
+// document.querySelector("h1") -> object // first macth agurata varanum
+// document.querySelectorAll("div.test") -> array
+// getElementsByTagName - array  
 
+
+//innerHTMl - show tag and text
+//innerText - > only show text
+
+
+// -> create one element method
+// const ele = document.createElement('p')
+//  ele.innerHTML  = "From Sever...";
+// const testEle = document.querySelector("div.test");
+// testEle.appendChild(ele);
+
+// append("firstName"); ->  text work agum and HTML ele , multipule element work agum 
+// appendChild("firstNmae"); -> text work agathu,  element work agum ,not muliple use panna only first ele
+
+
+//ele.setAttribute("type" ,"text")
+//ele.setAttribute("Name" ,"FullName")
+
+// Document.
+//  ^
+//  |
 //title
 //links
 //head

6dfc10a867920dacfef6b07faa2b10aa50f84ea4
Austinraj <malaraustinraja25@gmail.com>
2023-10-25 19:44:24 +0530

    commit

--- day 18/index.js
+++ day 18/index.js
@@ -1,7 +1,18 @@
 function clickMe(evt){
-    evt.target.inneText = "Clicked";
+    // evt.target.inneText = "Clicked";
+    console.log(evt);
     console.log("clicked !!");
 }
+
+const btn = document.getElementById("btn");
+// btn.onclick = clickMe;->  two same
+btn.addEventListener("click", clickMe);
+
+
+
+// btn.name = "austin"  ->same
+// btn.setAttribute("name", "raja");
+
 
 
 // function -> 

a30911f60f188782d3f4964de94abe3e4280d08e
Austinraj <malaraustinraja25@gmail.com>
2023-10-25 13:45:10 +0530

    coomit

---
+++ day 18/index.js
@@ -0,0 +1,10 @@
+function clickMe(evt){
+    evt.target.inneText = "Clicked";
+    console.log("clicked !!");
+}
+
+
+// function -> 
+// function with event
+// inline action definition
+// inline action with this

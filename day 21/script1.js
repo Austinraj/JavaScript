d9c1191bd9e447d772686148a980d385f68cee7d
Austinraj <malaraustinraja25@gmail.com>
2023-10-31 16:31:03 +0530

    commit

--- day 21/script1.js
+++ day 21/script1.js
@@ -4,3 +4,28 @@
 // Read -GET
 // Update -put
 // Delete - Delete
+
+
+
+// GET      URL: https://www.melivecode.com/api/users
+// POST     URL: https://www.melivecode.com/api/users/create
+// PUT      URL: https://www.melivecode.com/api/users/update
+// DELETE   URL: https://www.melivecode.com/api/users/delete
+
+const BASE_URL ="https://www.mecallapi.com/api/users";
+
+function SaveDetail(){
+    const http = new XMLHttpRequest();
+
+    http.onreadystatechange = function(){
+        const {readyState, status, responseText} = this;
+        if(readyState == 4 && status == 200){
+            const users =JSON.parse(responseText);
+            console.log(users);
+        }
+        http.open("GET", BASE_URL);
+        http.send();
+    }
+}
+
+SaveDetail();

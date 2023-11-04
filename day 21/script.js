68d57de7016263c4bace6763e0d9b84bae6f2dd6
Austinraj <malaraustinraja25@gmail.com>
2023-11-04 15:01:42 +0530

    commit

--- day 21/script.js
+++ day 21/script.js
@@ -3,7 +3,24 @@
 // console
 // document
 // setTimeout
+// web API -Application Programming Interface
+
+// Call back function 
+// it call Async
+
+// GEC -Globle Exection context
+
+// call Stack -read panni executed pannum
+// event Loop -
+// Callback Queue
 // web API
+
+console.log("first");
+document.getElementById("btn")
+.addEventListener("click", function click(){
+  console.log("clicked");
+});
+
 
 console.log("austin");
 

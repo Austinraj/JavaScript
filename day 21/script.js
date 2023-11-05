6e7ac9788dde9a5f8995381816955cc96e9d163e
Austinraj <malaraustinraja25@gmail.com>
2023-11-05 19:56:20 +0530

    commit

--- day 21/script.js
+++ day 21/script.js
@@ -16,6 +16,31 @@
 // Callback Queue
 // web API
 
+// 1 H1 => 4000 => Red
+
+// 2 H1 => 3000 => Green
+
+// 3 H1 => 5000 => Blue
+
+
+const first = document.getElementById("firstH1");
+const second = document.getElementById("firstH2");
+const thrid = document.getElementById("firstH3");
+
+setTimeout(()=>{
+  first.style.color = "red";
+}, 4000);
+
+setTimeout(()=>{
+  second.style.color = "green";
+}, 3000);
+setTimeout(()=>{
+  thrid.style.color = "blue";
+}, 5000);
+
+
+// callback hell problem
+
 console.log("first");
 document.getElementById("btn")
 .addEventListener("click", function click(){
@@ -34,3 +59,21 @@ console.log("zen");
 const arr = [1,2,3,4,5];
 arr.map((a) => a*1);
 console.log(arr);
+
+
+function isEvenOrOdd(num){
+  if(num % 2 === 0){
+    return "even";
+  }else{
+    return "odd";
+
+  }
+}
+
+// console.log(isEvenOrOdd(2));
+
+
+// error first Callback
+isEvenOrOdd(2 ,function(err,result){
+ 
+});

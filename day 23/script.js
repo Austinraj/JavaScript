273937e975d6cd568fda41473496c4bfad7e9b60
Austinraj <malaraustinraja25@gmail.com>
2023-11-07 17:15:56 +0530

    commit

--- day 23/script.js
+++ day 23/script.js
@@ -1,12 +1,25 @@
 // Fetch
 
 // fetch function use panni promies return pannum
+// next then throw pannaum na return use panni value throw panalam
 
-fetch("https://jsonplaceholder.typicode.com/posts")
+
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
 .then((res)=>{
+    // validation
+    if(!res.ok){
+        throw new Error(`request falied with ${res.status}`);
+    }else{
+        return res.json();
+    }
+})
+.then((res)=>{
+    console.log("Response==>");
     console.log(res);
+    
 })
 .catch((err)=>{
-    console.log(err);
+    console.error(err);
 
 })

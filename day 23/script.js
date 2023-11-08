61d2ff38637b687c2ee002f75c510a8c4a3441ae
Austinraj <malaraustinraja25@gmail.com>
2023-11-08 18:04:18 +0530

    commit

--- day 23/script.js
+++ day 23/script.js
@@ -5,21 +5,58 @@
 
 
 
-fetch("https://jsonplaceholder.typicode.com/posts/1")
-.then((res)=>{
-    // validation
-    if(!res.ok){
-        throw new Error(`request falied with ${res.status}`);
-    }else{
-        return res.json();
-    }
-})
-.then((res)=>{
-    console.log("Response==>");
-    console.log(res);
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+// .then((res)=>{
+//     // validation
+//     if(!res.ok){
+//         throw new Error(`request falied with ${res.status}`);
+//     }else{
+//         return res.json();
+//     }
+// })
+// .then((res)=>{
+//     console.log("Response==>");
+//     console.log(res);
     
-})
-.catch((err)=>{
-    console.error(err);
+// })
+// .catch((err)=>{
+//     console.error(err);
 
-})
+// })
+
+
+
+
+function handleResponse(res2){
+    if(!res2.ok){
+        throw new Error (`Request failed with ${res2.status}`);
+    }
+    return res2.json();
+}
+
+function handleError(err){
+    console.log("Error ===>");
+    console.log(err);
+}
+const getPost = (postId)=> fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`).then(handleResponse);
+const getUser = (userId)=> fetch(`https://jsonplaceholder.typicode.com/users/${userId}`).then(handleResponse);
+const getComment = (postId)=> fetch(`https://jsonplaceholder.typicode.com/post/${postId}/comments`).then(handleResponse);
+
+
+
+
+// Async Await is real time la use pannurom
+
+const getData = async () =>{
+    try {
+        const post = await getPost(1);
+        const user = await getUser(post.userId);
+        const comment = await getComment(post.id);
+    
+        console.log(post,user,comment);
+    }catch(err){
+        console.error(err);
+    }
+};
+
+getData();

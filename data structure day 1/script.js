6376d0f67f14dae2be0913c610b60baa773086e1
Austinraj <malaraustinraja25@gmail.com>
2023-11-10 22:37:53 +0530

    commit

---
+++ data structure day 1/script.js
@@ -0,0 +1,55 @@
+// let arr =[1,2,3,5];
+
+// for(i in arr){
+//     console.log(i);
+// }
+
+// Problem #1
+// find the maximum consecutive ones 
+
+// let arr1 =[1,1,0,1,1,1,0,0,0,1];
+
+// function briny(nums){
+//     var count = 0;
+//     var maxcount =0;
+
+//     for(var i=0;i<nums.length;i++){
+//         if(nums[i] ===1){
+//             count++;
+//         }
+//         else{
+//              maxcount =Math.max(maxcount,count);
+//              count =0;
+//         }
+        
+//     }
+//     return Math.max(maxcount,count);
+// }
+// console.log(briny(arr1));
+
+
+// Problem #2
+// Given array num of integers, return how many of them have even number of digits
+
+num1 =[15,111,654,4656];
+
+    var ans =0;
+    var count =0;
+    for(var i=0;i<num1.lengt;i++){
+        var temp =num1[i];
+        while(temp !=0){
+            temp = temp/10
+            count++;
+        }
+        if(num1[i] %2 ==0){
+            count =0;
+        }
+        
+    }
+    console.log(ans);
+
+
+
+
+
+

31c2b2d859e6f0586a31d85a5250dc7252ce9e55
Austinraj <malaraustinraja25@gmail.com>
2023-12-09 15:26:41 +0530

    commit

--- webkata/script.js
+++ webkata/script.js
@@ -31,10 +31,45 @@
 with id 'selectedOption' using Javascript. 
 Note: Values are changed dynamically for evaluation */
 
-function displaySelectedOption() {
-    const vale1 = document.getElementsByName("option");
-    console.log(vale1);
-    // console.log(vale1);
-    document.getElementById("selectedOption").innerHTML = vale1;
-}
-displaySelectedOption();
+// function displaySelectedOption() {
+//     const vale1 = document.getElementsByName("option");
+//     console.log(vale1);
+//     // console.log(vale1);
+//     document.getElementById("selectedOption").innerHTML = vale1;
+// }
+// displaySelectedOption();
+
+// 11.
+// In cludes method use panna output => that world irutha true or fales
+// function checkString(){
+
+//     const inpEl = document.getElementById("in1").value;
+
+//     const vale = inpEl.includes('Java');
+
+//     document.getElementById("result").innerHTML = vale ;
+// }
+// checkString();
+
+
+// 15.
+// function compare(){
+//     const inp1EL = document.getElementById("in1").value ;
+//     const inp2EL = document.getElementById("in2").value ;
+//     if( Number(inp1EL) < Number(inp2EL)){
+//         document.getElementById("result").innerHTML = true;
+//     }
+//     else{
+//         document.getElementById("result").innerHTML = false;
+//     }
+    
+// }
+// compare();
+
+//16.
+const changeEl = document.getElementsByClassName("changeMeToBlue") ;
+document.getElementById("id").hidden
+
+
+
+// document.getElementById("id").style.textAlign = "center" ;

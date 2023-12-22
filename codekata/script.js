98949392f822f82db326d92b9b2ad12328f52b15
Austinraj <malaraustinraja25@gmail.com>
2023-12-22 14:50:33 +0530

    commit

---
+++ codekata/script.js
@@ -0,0 +1,28 @@
+/*
+
+You are provided with a number "N", Find the Nth term of the series: 1, 4, 9, 16, 25, 36, 49, 64, 81, .......
+
+(Print "Error" if N = negative value and 0 if N = 0).
+
+Input Description:
+An integer N is provided to you as the input.
+
+Output Description:
+Find the Nth term in the provided series.
+
+Sample Input :
+18
+Sample Output :
+324
+
+
+*/
+num = [1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+console.log(9*4)
+num.forEach((value, index) => {
+    if(index%2==0){
+        let num = value * 4  
+        console.log(num);
+    }
+});
